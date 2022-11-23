60a2ceb483a54a06278846c5ed6cffba3141e707
Ulageshgit <117634586+Ulageshgit@users.noreply.github.com>
2022-11-23 11:16:16 +0530

    Add files via upload

---
+++ Kminelibrary/Contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "info");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$name = $_REQUEST['name'];
+		$email = $_REQUEST['email'];
+		$PhoneNo = $_REQUEST['PhoneNo'];
+		$message = $_REQUEST['message'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO contact VALUES ('$$name',
+			'$email','$PhoneNo','$message')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			echo nl2br("\n$name\n $$email\n "
+				. "$PhoneNo\n $message");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
